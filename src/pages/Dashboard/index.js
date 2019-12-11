d0f86d7ccafdc67dc4cf4311e1307a20980205c1
Davi Holanda <daviht7@gmail.com>
2019-12-10 23:16:20 -0300

    estilização dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,36 @@
 import React from "react";
 import api from "~/services/api";
 
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { Container, Time } from "./styles";
 
 export default function Dashboard() {
-  api.get("appointment");
-  return <h1> Dashboard </h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={44} color="#fff" />
+        </button>
+        <strong>31 de maio</strong>
+        <button type="button">
+          <MdChevronRight size={44} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Kelcya Holanda</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Maria Socorro</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
